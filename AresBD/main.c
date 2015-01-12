252d80abd96b881600ac670057e837d6dd21d4bb
Stsosz <sabergelpreston@icloud.com>
2015-01-12 15:49:18 -0500

    Initial Commit

---
+++ AresBD/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AresBD
+//
+//  Created by Seamus Abergel-Preston on 2015-01-12.
+//  Copyright (c) 2015 Stsosz Systems. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
