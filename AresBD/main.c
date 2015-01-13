e78ba5693f2611a0d161b753795f631b07b82538
Stsosz <sabergelpreston@icloud.com>
2015-01-12 20:04:27 -0500

    First code.

--- AresBD/main.c
+++ AresBD/main.c
@@ -5,11 +5,50 @@
 //  Created by Seamus Abergel-Preston on 2015-01-12.
 //  Copyright (c) 2015 Stsosz Systems. All rights reserved.
 //
-
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Dev!\n");
+int
+main (int argc, char **argv)
+{
+    int aflag = 0;
+    int bflag = 0;
+    char *cvalue = NULL;
+    int index;
+    int c;
+    
+    opterr = 0;
+    while ((c = getopt (argc, argv, "abc:")) != -1)
+        switch (c)
+    {
+        case 'a':
+            aflag = 1;
+            break;
+        case 'b':
+            bflag = 1;
+            break;
+        case 'c':
+            cvalue = optarg;
+            break;
+        case '?':
+            if (optopt == 'c')
+                fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+            else if (isprint (optopt))
+                fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+            else
+                fprintf (stderr,
+                         "Unknown option character `\\x%x'.\n",
+                         optopt);
+            return 1;
+        default:
+            abort ();
+    }
+    printf ("aflag = %d, bflag = %d, cvalue = %s\n",
+            aflag, bflag, cvalue);
+    
+    for (index = optind; index < argc; index++)
+        printf ("Non-option argument %s\n", argv[index]);
     return 0;
 }
