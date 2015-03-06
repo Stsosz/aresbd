c4228aa16d89ff412b269faef791758879d1d316
Stsosz <sabergelpreston@icloud.com>
2015-03-06 18:44:18 -0500

    remove unneeded code

--- abdconsole.bash
+++ abdconsole.bash
@@ -7,13 +7,6 @@
 #  Don't be an asshole. Don't use this without permission. I'm not responsible
 #  for any stupid stuff you do with AresBD.
 
-#Make sure that user uses sudo
-#if [[ $UID != 0 ]]; then
-#echo "Please run again with sudo: "
-#echo "sudo $0 $*"
-#exit 1
-#fi
-
 source ~/aresbd/aresbd.conf
 PS3=$MENUPROMPT
 
